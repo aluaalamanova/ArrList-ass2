c1ac743c8e23b1d58e00dd6a3b1183d6132d93fa
ADMIN <221322@astanait.edu.kz>
2023-04-28 18:43:35 +0600

    sort subArr method in arraylist

---
+++ src/MyLinkedListStack.java
@@ -0,0 +1,7 @@
+import java.util.EmptyStackException;
+public class MyLinkedListStack<T>{
+    private MyLinkedList<T> linkedList;
+    public MyLinkedListStack() {
+        linkedList= new MyLinkedList<T>();
+    }
+}
