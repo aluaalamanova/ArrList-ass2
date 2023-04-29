e663235ffb74c289c2cfe893aa164711a2078977
ADMIN <221322@astanait.edu.kz>
2023-04-29 23:34:16 +0600

    size() method

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -2,18 +2,24 @@ import java.util.EmptyStackException;
 public class MyLinkedListStack<T>{
     private MyLinkedList<T> linkedList;
     public MyLinkedListStack() {
-        linkedList= new MyLinkedList<T>();
+        linkedList= new MyLinkedList<>();
     }
     public void push (T element){
         linkedList.add(element);
     }
     public T pop(){
-        return linkedList.remove(linkedList.size() - 1);
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return linkedList.remove(size() - 1);
     }
     public boolean isEmpty(){
         if (linkedList.size()==0){
             return true;
         }
         return false;
+    }
+    public int size(){
+        return linkedList.size();
     }
 }
