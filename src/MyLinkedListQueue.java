260d334413b4edc91010439ef316c6af2da4ebe6
ADMIN <221322@astanait.edu.kz>
2023-05-01 16:26:22 +0600

    importing exception class and declaring instance of class

---
+++ src/MyLinkedListQueue.java
@@ -0,0 +1,9 @@
+import java.util.NoSuchElementException;
+
+public class MyLinkedListQueue<T> {
+    private MyLinkedList<T> list;
+    public MyLinkedListQueue() {
+        list = new MyLinkedList<T>();
+    }
+
+}
