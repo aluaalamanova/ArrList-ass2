ed66f61c5eb5a6d98254bc1516a1145085e5be49
ADMIN <221322@astanait.edu.kz>
2023-05-01 17:10:54 +0600

    push() and pop() methods in MyArrayListStack

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,2 +1,16 @@
-public class MyArrayListStack {
+import java.util.EmptyStackException;
+
+public class MyArrayListStack<T> {
+    private MyArrayList<T> list;
+
+    public MyArrayListStack() {
+        list = new MyArrayList<T>();
+    }
+    public void push(T element) {
+        list.add(element);
+    }
+    public T pop() {
+        return list.remove(list.size() - 1);
+    }
+
 }
